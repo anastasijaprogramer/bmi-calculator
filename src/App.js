ac52555b32690de22160b2f164945ef43154f1f1
anastasija radulovic <anastasija.radulovic@tasteatlas.com>
2023-11-08 11:54:24 +0100

    build bmi calculator project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useMemo } from "react";
 
-function App() {
+const DEFAULT_WEIGHT = 50;
+const DEFAULT_HEIGHT = 140;
+
+function App()
+{
+  const [height, setHeight] = useState(DEFAULT_HEIGHT);
+  const [weight, setWeight] = useState(DEFAULT_WEIGHT);
+
+  const handleWeightChange = (e) =>
+  {
+    setWeight(e.target.value);
+  };
+
+  const handleHeightChange = (e) =>
+  {
+    setHeight(e.target.value);
+  };
+
+  const output = useMemo(() =>
+  {
+    const calculatedHeight = height / 100;
+    return (weight / (calculatedHeight * calculatedHeight)).toFixed(1);
+  }, [weight, height])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main>
+      <h1>BMI CALCULATOR</h1>
+      <div className='input-section'>
+
+        <p className="slider-output">Weight: {weight} kg</p>
+        <input
+          onChange={handleWeightChange}
+          className="input-slider"
+          type="range"
+          step="1"
+          min="40"
+          max="220" />
+
+        <p className="slider-output">Height: {height} cm</p>
+        <input
+          onChange={handleHeightChange}
+          className="input-slider"
+          type="range"
+          step="1"
+          min="140"
+          max="250" />
+
+        <div className="output-section">
+          <p>Your BMI is</p>
+          <p className="output">{output}</p>
+        </div>
+      </div>
+    </main>
+
   );
 }
 
